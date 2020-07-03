95b9cd13333550c504c74d11f91b49b7d4d4d891
KingMedieval <tshimada@purdue.edyu>
2020-07-03 19:35:01 +0900

    fixed reaction

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ client.on("message", async (message) => {
 });
 
 client.on('messageReactionAdd', (reaction, user) => {
-  if(reaction.emoji.name == '❌' && user.id != '717346833101488140' && message.channel.nsfw) {
+  if(reaction.emoji.name == '❌' && user.id != '717346833101488140') {
     reaction.message.delete(reaction.message.lastMessageID);
   }
 });
