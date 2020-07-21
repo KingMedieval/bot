86f85f1d45ad3f95b677c609b3a46fde344bc823
KingMedieval <tomoari.shimada@gmail.com>
2020-07-22 01:05:42 +0900

    experimental lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -7,14 +7,21 @@ module.exports = {
     name: "lyrics",
     description: "Displays lyrics of given song name",
     async execute(message, args) {
+        const queue = message.client.queue.get(message.guild.id);
 
         if (!KSOFT_API_KEY)
             return message.reply("Missing Ksoft api key in config").catch(console.error);
 
-        if (!args.length)
+        if (!args.length && !queue)
             return message.reply(`Usage: ${message.client.prefix}lyrics <Song Name>`).catch(console.error);
+        else if (!args.length) {
+          const song = queue.songs[0];
+          const search = `${song.title}`;
+        }
+        else {
+          const search = args.join(" ");
+        }
 
-        const search = args.join(" ");
         let response = await fetch('https://api.ksoft.si/lyrics/search?q=' + search, {
             headers: {
                 'Content-Type': 'application/json',
