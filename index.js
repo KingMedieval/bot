69861e0d96e9136d19a1eccab718be5d95af42b0
KingMedieval <63794592+KingMedieval@users.noreply.github.com>
2021-04-06 04:15:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -91,8 +91,8 @@ client.on("message", async (message) => {
 });
 
 client.on('messageReactionAdd', (reaction, user) => {
-  if(reaction.emoji.name == '❌' && user.id != '717346833101488140') {
-    if (reaction.message.author.id == '717346833101488140') {
+  if(reaction.emoji.name == '❌' && user.id != '739138646225059924') {
+    if (reaction.message.author.id == '739138646225059924') {
       reaction.message.delete(reaction.message.lastMessageID);
     }
   }
